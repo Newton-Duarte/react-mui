8aea447ecbf74b2c4c31e742616881e2313c485b
Newton Duarte <newton_@hotmail.com>
2022-10-22 06:46:42 -0300

    feat: add mui image list component

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,10 @@
 // import { MuiAutocomplete } from './components/MuiAutocomplete';
 // import { MuiLayout } from './components/MuiLayout';
 // import { MuiCard } from './components/MuiCard';
+// import { MuiAccordion } from './components/MuiAccordion';
 
 import './App.css';
-import { MuiAccordion } from './components/MuiAccordion';
+import { MuiImageList } from './components/MuiImageList';
 
 function App() {
   return (
@@ -27,7 +28,8 @@ function App() {
       {/* <MuiAutocomplete /> */}
       {/* <MuiLayout /> */}
       {/* <MuiCard /> */}
-      <MuiAccordion />
+      {/* <MuiAccordion /> */}
+      <MuiImageList />
     </div>
   );
 }
