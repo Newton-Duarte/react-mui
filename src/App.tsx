decb8fa51f5730dcaa89bf6c7d71791068097c47
Newton Duarte <newton_@hotmail.com>
2022-11-01 06:23:03 -0300

    feat: add mui alert component

--- src/App.tsx
+++ src/App.tsx
@@ -22,9 +22,10 @@
 // import { MuiList } from './components/MuiList';
 // import { MuiChip } from './components/MuiChip';
 // import { MuiTooltip } from './components/MuiTooltip';
+// import { MuiTable } from './components/MuiTable';
 
 import './App.css';
-import { MuiTable } from './components/MuiTable';
+import { MuiAlert } from './components/MuiAlert';
 
 function App() {
   return (
@@ -53,7 +54,8 @@ function App() {
       {/* <MuiList /> */}
       {/* <MuiChip /> */}
       {/* <MuiTooltip /> */}
-      <MuiTable />
+      {/* <MuiTable /> */}
+      <MuiAlert />
     </div>
   );
 }
